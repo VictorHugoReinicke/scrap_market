94688bad5d6fc7a61a1d6d80323581da962053f0
Victor Hugo Reinicke <pombewcraft@gmail.com>
2025-05-01 22:31:46 -0300

    feat, adding some functions to get more info about the products

--- scrap.py
+++ scrap.py
@@ -1,6 +1,9 @@
 from curl_cffi import requests
 from bs4 import BeautifulSoup
 import asyncio
+from tqdm import tqdm
+import json
+import pandas as pd
 
 async def search_items(search_item, itens):
     final_links = []
@@ -21,8 +24,35 @@ async def url(search_item, item_number):
     desde = ((page_number - 1) * 50) + 1
     return(f"https://lista.mercadolivre.com.br/{search_item}/_Desde_{desde}_AGE*GROUP_6725189_NoIndex_True")
 
+async def parser_product(links):
+    products = []
+    for link in tqdm(links):
+        response = requests.request("GET", link, impersonate="chrome")
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        data_json = json.loads(
+            soup.find("script", {"type": "application/ld+json"}).string
+        )
+        product = {
+            "preco": data_json["offers"]["price"],
+            "titulo": data_json["name"],
+            "imagem": data_json["image"],
+        }
+        products.append(product)
+
+    print(len(products))
+    return products
+
+def to_save(products):
+
+    df = pd.DataFrame(products)
+
+    return df.to_csv("mercado_livre/dados_raspagem.csv", index=False)
+
 async def main():
-    return await search_items('graxa', 100)
+    links = await search_items('graxa', 100)
+    products = await parser_product(links)
+    to_save(products)
 
 if __name__ == "__main__":
     asyncio.run(main())
